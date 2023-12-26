3c34b2c5a61eead04af2707cb7b9404ea0ad3320
xiaoxian521 <1923740402@qq.com>
2023-12-26 20:26:19 +0800

    chore: update `playgrounds`

--- playgrounds/auto-import-demo/src/main.ts
+++ playgrounds/auto-import-demo/src/main.ts
@@ -1,11 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// If you want to use ElTable, import it.
-import "element-plus/es/components/table/style/css";
-import "element-plus/es/components/pagination/style/css";
-import "element-plus/es/components/loading/style/css";
-
+import "@pureadmin/table/dist/style.css";
 import PureTable from "@pureadmin/table";
 
 const app = createApp(App);
--- playgrounds/whole-import-demo/src/main.ts
+++ playgrounds/whole-import-demo/src/main.ts
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
 
+import "@pureadmin/table/dist/style.css";
 import PureTable from "@pureadmin/table";
 
 const app = createApp(App);
